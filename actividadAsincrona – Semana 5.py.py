803b95d2fe14c99720f775027c0af7ed2442858b
carloslandaverde3333 <125209964+carloslandaverde3333@users.noreply.github.com>
2023-02-27 12:44:14 -0600

    Add files via upload

---
+++ actividadAsincrona – Semana 5.py.py
@@ -0,0 +1,103 @@
+print("Hola ingeniero espero tenga un buen dia")
+print("bienvenido a la actividad 5,semana5 de fundamentos de programacion")
+
+nombre_1 = "Felix Mauricio"
+apellido_1 = "Palacios Tejada"
+edad_1 = 17
+sexo_1 = "Masculino"
+print(" ")
+print ("Integrante numero uno:")
+print ("Nombre:", nombre_1 + " , Apellido:", apellido_1 + ", Sexo:", sexo_1 + " ,Edad:", edad_1)
+
+ 
+nombre_2 = "Carlos Ernesto"
+apellido_2= "Landaverde Quintanilla"
+edad_2 = 17
+sexo_2 = "Masculino"
+print(" ")
+print ("Integrante numero dos:")
+print ("Nombre", nombre_2 +" , Apellido:" ,apellido_2 + ", Sexo:", sexo_2 + ",Edad:", edad_2)
+
+
+nombre_3 = "Ivan Anderson"
+apellido_3 = "Membreño Guardado"
+edad_3 = 18
+sexo_3 = "Masculino"
+print(" ")
+print ("Integrante numero tres:")
+print ("Nombre:", nombre_3 +" , Apellido:", apellido_3 + ", Sexo:", sexo_3 + ",Edad:", edad_3)
+
+nombre_4 = "Marilyn Alexandra"
+apellido_4 = "Menjivar Fuentes"
+edad_4 = 17
+sexo_4 = "Femenino"
+print(" ")
+print ("Integrante numero cuatro:")
+print ("Nombre:", nombre_4 +" , Apellido:", apellido_4 + " , Sexo:", sexo_4 + ",Edad:", edad_4)
+
+nombre_5 = "Sonia Elizabeth"
+apellido_5 = "Abrego Nuñes"
+edad_5 = 19
+sexo_5 = "Femenino"
+print(" ")
+print ("Integrante numero cinco:")
+print ("Nombre:", nombre_5 + ", Apellido:", apellido_5 + ", Sexo:", sexo_5 + ",Edad:", edad_5)
+
+mensaje = "Hola ing soy el integrante numero uno Felix"
+buscarSubCadena = mensaje.find("Felix")
+
+print (" ")
+print ("La SubCadena es:", buscarSubCadena)
+
+print("promedio de edades") 
+suma_edades = edad_1 + edad_2 + edad_3 + edad_4 + edad_5
+promedio = suma_edades/5
+
+print ("La edad promedio de los integrantes del grupo es:",promedio)
+
+print(" ")
+
+print("Contenacio de nombres y apellidos de cada integrante")
+
+nombre_completo_1 = nombre_1 + (" ") + apellido_1
+nombre_completo_2 = nombre_2 + (" ") + apellido_2
+nombre_completo_3 = nombre_3 + (" ") + apellido_3
+nombre_completo_4 = nombre_4 + (" ") + apellido_4
+nombre_completo_5 = nombre_5 + (" ") + apellido_5
+print ("Nombre completo del integrante numero uno: ", nombre_completo_1 )
+print ("Nombre completo del integrante numero dos: ", nombre_completo_2 )
+print ("Nombre completo del integrante numero tres: ", nombre_completo_3 )
+print ("Nombre completo del integrante numero cuatro: ", nombre_completo_4 )
+print ("Nombre completo del integrante numero cinco: ", nombre_completo_5 ) 
+print(" ")
+
+print("Edad a la que tendrian que salir cada integrante")
+
+print ("Edad a la que tendria que salir el integrante numero uno:", edad_1 + 5)
+print ("Edad a la que tendria que salir el integrante numero dos:", edad_2 + 5)
+print ("Edad a la que tendria que salir el integrante numero tres:", edad_3 + 5)
+print ("Edad a la que tendria que salir el integrante numero cuatro:", edad_4 + 5)
+print ("Edad a la que tendria que salir el integrante numero cinco:", edad_5 + 5)
+print (" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
